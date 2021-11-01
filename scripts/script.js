61921085cee005dc68c46c9d5a1f2ff51ccbb7c5
idmx <slavyan1507@mail.ru>
2021-11-01 17:31:55 +0300

    feat: добавить программную часть редактирования профиля и адаптировать сайт

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 let pop_exit = document.querySelector('.popup__close');
 let pop_open = document.querySelector('.profile__edit-button');
-let pop_save = document.querySelector('.popup__save');
+let popup_form = document.querySelector('.popup__form');
 let state = 0;
 
-function popup(state) {
+function popup() {
   let pop = document.querySelector('.popup');
-  if (state) {
-    state = 0;
-    let popup_name = document.querySelector('.popup__name');
-    let popup_about = document.querySelector('.popup__about');
-    let name = document.querySelector('.profile__name');
-    let about = document.querySelector('.profile__about');
-    name.textContent = popup_name.value;
-    about.textContent = popup_about.value;
-    pop.classList.toggle('popup_opened');
-  }
-  else {
-    pop.classList.toggle('popup_opened');
-  }
+  pop.classList.toggle('popup_opened');
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  let pop = document.querySelector('.popup');
+  let popup_name = document.querySelector('.popup__name');
+  let popup_about = document.querySelector('.popup__about');
+  let name = document.querySelector('.profile__name');
+  let about = document.querySelector('.profile__about');
+  name.textContent = name.title = (popup_form[0].value || "Аноним");
+  about.textContent = about.title = (popup_form[1].value || "Информация отсутствует");
+  pop.classList.toggle('popup_opened');
+}
 
-pop_open.addEventListener('click', {handleEvent: popup, state: 0});
-pop_exit.addEventListener('click', {handleEvent: popup, state: 0});
-pop_save.addEventListener('click', {handleEvent: popup, state: 1});
+pop_open.addEventListener('click', popup);
+pop_exit.addEventListener('click', popup);
+popup_form.addEventListener('submit', formSubmitHandler);
