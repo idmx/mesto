9796e5aabc316556e46dc7b14f8f2fe3a7d20c40
idmx <slavyan1507@mail.ru>
2021-11-04 23:13:47 +0300

    fix: исправлены ошибки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,27 @@
-let pop_exit = document.querySelector('.popup__close');
-let pop_open = document.querySelector('.profile__edit-button');
-let popup_form = document.querySelector('.popup__form');
-let state = 0;
+let popExit = document.querySelector('.popup__close');
+let popOpen = document.querySelector('.profile__edit-button');
+let popupForm = document.querySelector('.popup__form');
+let pop = document.querySelector('.popup');
+let name = document.querySelector('.profile__name');
+let about = document.querySelector('.profile__about');
 
-function popup() {
-  let pop = document.querySelector('.popup');
+function popupOpen() {
+  popupForm[0].value = name.textContent;
+  popupForm[1].value = about.textContent;
+  pop.classList.toggle('popup_opened');
+}
+
+function popupClose() {
   pop.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
-  let pop = document.querySelector('.popup');
-  let popup_name = document.querySelector('.popup__name');
-  let popup_about = document.querySelector('.popup__about');
-  let name = document.querySelector('.profile__name');
-  let about = document.querySelector('.profile__about');
-  name.textContent = name.title = (popup_form[0].value || "Аноним");
-  about.textContent = about.title = (popup_form[1].value || "Информация отсутствует");
+  name.textContent = popupForm[0].value;
+  about.textContent = popupForm[1].value;
   pop.classList.toggle('popup_opened');
 }
 
-pop_open.addEventListener('click', popup);
-pop_exit.addEventListener('click', popup);
-popup_form.addEventListener('submit', formSubmitHandler);
+popOpen.addEventListener('click', popupOpen);
+popExit.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', formSubmitHandler);
